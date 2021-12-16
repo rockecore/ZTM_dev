1e47583f2030f67a573d3eada0f68b00f719df34
Rocky Barker <rocky.barker@imdexlimited.com>
2021-12-17 07:31:17 +1300

    adding starting project

---
+++ script.js
@@ -0,0 +1,77 @@
+// 4 + 3;
+
+// if (4+3 === 7) {
+// 	// alert("You are smart");
+// 	console.log("Hellooooooo")
+// }
+
+
+// functions
+
+
+// function sayHello() {
+// 	console.log("Hello");
+// }
+
+// sayHello();
+
+// var sayBye = function() {
+// 	console.log("Bye");
+// }
+
+// sayBye();
+
+// var sayLa = "laaaaaaaaa"
+// var sayTeDa = "teeeedaaaaa"
+
+// function sing(la, da) {
+// 	console.log(la);
+// 	console.log(da);
+
+// }
+
+// // sing(sayLa, sayTeDa)
+
+
+// function sing(song) {
+// 	console.log(song);
+// }
+
+// sing("blalalallalala")
+
+
+// function multiply(a, b) {
+// 	return [a * b, a, b]
+// }
+
+// const [x, y, z] = multiply(7,)
+
+// parameteters are a and b
+
+
+// ############## Data structures ############## 
+// arrays
+// var list = ["tiger", "cat", "bear", "bird"]
+// console.log(list[0]);
+
+// list.shift(); to remove the first
+// list.pop(); to remove back
+// list.concat([]); to add a list to end of list, not in-place
+// list.sort(); to alphabetize
+// list.push(); not in-place
+
+
+// objects (properties and values of a dictionary)
+var user = {
+	name: "John",
+	age: 34,
+	hobby: "Soccer",
+	isMarried: false,
+};
+// to call
+user.age 
+
+// to add
+user.favoriteFood = "spinach";
+
+// to change just set equal to something else
